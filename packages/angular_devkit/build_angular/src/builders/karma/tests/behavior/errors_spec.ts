9ba714218cf8d1a215c6082cf5ddc51b9f26b75b
Jan Martin <jan.krems@gmail.com>
2025-02-13 11:26:16 -0800

    fix(@angular-devkit/build-angular): apply source maps to failures
    Failed assertions and other errors that happen during test execution
    should be attributed to the source file, not the JS bundles. This was
    previously only the case for the browser builder but broken when using
    application builder with Karma.
    
    Fixes https://github.com/angular/angular-cli/issues/29129

--- packages/angular_devkit/build_angular/src/builders/karma/tests/behavior/errors_spec.ts
+++ packages/angular_devkit/build_angular/src/builders/karma/tests/behavior/errors_spec.ts
@@ -9,6 +9,31 @@
 import { execute } from '../../index';
 import { BASE_OPTIONS, KARMA_BUILDER_INFO, describeKarmaBuilder } from '../setup';
 
+class StdoutCapture {
+  private originalWrite: typeof process.stdout.write;
+  private stdout: Array<Buffer> = [];
+
+  constructor() {
+    this.originalWrite = process.stdout.write;
+    process.stdout.write = ((
+      str: Uint8Array | string,
+      encoding?: BufferEncoding,
+      cb?: (err?: Error) => void,
+    ) => {
+      this.stdout.push(Buffer.from(str));
+      return this.originalWrite.call(process.stdout, str, encoding, cb);
+    }) as typeof process.stdout.write;
+  }
+
+  toString() {
+    return Buffer.concat(this.stdout).toString();
+  }
+
+  release() {
+    process.stdout.write = this.originalWrite;
+  }
+}
+
 describeKarmaBuilder(execute, KARMA_BUILDER_INFO, (harness, setupTarget) => {
   describe('Behavior: "Errors"', () => {
     beforeEach(async () => {
@@ -27,6 +52,36 @@ describeKarmaBuilder(execute, KARMA_BUILDER_INFO, (harness, setupTarget) => {
       });
 
       expect(result?.success).toBeFalse();
+    });
+
+    fit('should report failures with correct source locations', async () => {
+      await harness.writeFiles({
+        './src/app/app.component.spec.ts': `
+            import { AppComponent } from './app.component';
+
+            describe('Forced test failure', () => {
+              it('makes an invalid call', () => {
+                // @ts-ignore
+                expect(AppComponent()).toBe(true);
+              });
+            });`,
+      });
+
+      harness.useTarget('test', {
+        ...BASE_OPTIONS,
+      });
+
+      const stdout = new StdoutCapture();
+      try {
+        const { result } = await harness.executeOnce({
+          outputLogsOnFailure: false,
+        });
+        expect(result?.success).toBeFalse();
+      } finally {
+        stdout.release();
+      }
+
+      expect(stdout.toString()).toMatch(/src\/app\/app\.component\.spec\.ts:7/);
     });
   });
 });
